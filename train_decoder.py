04b93501413aa063be493dd5d1fe03aaadfd7832
Cameron Reid <csreid.cr@gmail.com>
2026-02-17 15:01:17 -0600

    Actually use weighted loss

--- train_decoder.py
+++ train_decoder.py
@@ -242,9 +242,9 @@ def fit_batch(model, optimizer, criterion, batch, device):
 	# Compute losses
 	#fd_loss = criterion(h_t_next_pred, h_t_next)
 	fd_loss = 0
-	decoder_loss_t = criterion(state_pred, state_t)
+	#decoder_loss_t = criterion(state_pred, state_t)
 	#decoder_loss_t_next = criterion(state_next_pred, state_t_next)
-	decoder_loss = decoder_loss_t #+ decoder_loss_t_next
+	#decoder_loss = decoder_loss_t #+ decoder_loss_t_next
 
 	# Compute per-component decoder losses (state has 11 dims: pos(3), vel(3), flipped(1), orient(4))
 
@@ -253,6 +253,8 @@ def fit_batch(model, optimizer, criterion, batch, device):
 	vel_loss_t = 10 * criterion(state_pred[..., 3:6], state_t[..., 3:6]) # magnify velocity loss
 	flipped_loss_t = criterion(state_pred[..., 6:7], state_t[..., 6:7])
 	orient_loss_t = criterion(state_pred[..., 7:], state_t[..., 7:])
+
+	decoder_loss = torch.sum([pos_loss_t, vel_loss_t, flipped_loss_t, orient_loss_t])
 
 #	#pos_loss_t_next = criterion(state_next_pred[..., :3], state_t_next[..., :3])
 #	vel_loss_t_next = criterion(
